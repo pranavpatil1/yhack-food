0c5874547f8589d463112074ef327c5478878207
Pranav Patil <4pranavpatil@gmail.com>
2020-11-07 14:43:48 -0800

    Added database connection

--- app.py
+++ app.py
@@ -1,35 +1,32 @@
 from flask import Flask, render_template
 from werkzeug.exceptions import abort
+import sqlalchemy
+import urllib
 
 app = Flask(__name__)
 
-# database code
-posts = [
-    {
-        'id': 0,
-        'title': 'big tasty snack',
-        'created': '11/7'
-    },
-    {
-        'id': 1,
-        'title': 'potato',
-        'created': '11/5'
-    }
-]
-
 def get_db_connection():
-    #conn = sqlite3.connect('database.db')
-    #conn.row_factory = sqlite3.Row
-    #return conn
-    return None
+    username = 'postgres'  # DB username
+    password = 'mitsucks'  # DB password
+    host = '35.236.20.170'  # Public IP address for your instance
+    port = '5432'
+    database = 'postgres'  # Name of database ('postgres' by default)
 
+    db_url = 'postgresql+psycopg2://{}:{}@{}:{}/{}'.format(
+        username, password, host, port, database)
+
+    engine = sqlalchemy.create_engine(db_url)
+
+    conn = engine.connect()
+    return conn
 
 def get_post(post_id):
-    # conn = get_db_connection()
-    # post = conn.execute('SELECT * FROM posts WHERE id = ?',
-    #                    (post_id,)).fetchone()
-    # conn.close()
-    post = posts[post_id] if post_id < len(posts) else None
+    conn = get_db_connection()
+        # urllib.parse.quote('/', safe='')
+    post = conn.execute('SELECT * FROM rest WHERE name = ?',
+                        (post_id,)).fetchone()
+    conn.close()
+
     if post is None:
         abort(404)
     return post
@@ -40,9 +37,12 @@ def index():
 
 @app.route('/eat/')
 def eat():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM rest LIMIT 5').fetchall()
+    conn.close()
     return render_template('eat.html', posts=posts)
 
-@app.route('/restaurant/<int:post_id>')
+@app.route('/restaurant/<string:post_id>')
 def restaurant(post_id):
     post = get_post(post_id)
     return render_template('restaurant.html', post=post)
