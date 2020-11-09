81e00e52d98de6bcfb04fbde82b7f977e946b4cd
Pranav Patil <4pranavpatil@gmail.com>
2020-11-08 20:06:06 -0800

    Added creating chef

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 import sqlalchemy
 import urllib
@@ -30,6 +30,10 @@ def get_post(post_id):
         abort(404)
     return post
 
+def formatPhone(phone):
+    phone = phone[0:3] + "-" + phone[3:6] + "-" + phone[6:]
+    return phone
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -37,12 +41,16 @@ def index():
 @app.route('/eat/')
 def eat():
     conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM rest LIMIT 5').fetchall()
+    posts = conn.execute('SELECT * FROM rest').fetchall()
     conn.close()
     posts_parsed = []
     for row in posts:
         d = dict(row.items())
         d['id'] = urllib.parse.quote(d['name'], safe='')
+        d['menu'] = ". ".join(d['menu'][:40].split(";")[:-1])
+        d['phone'] = formatPhone(str(d['phone']))
+        d['tags'] = d['tags'].split(", ")
+        d['full_add'] = ", ".join(d['full_add'].split(", ")[:3])
         posts_parsed.append(d)
     return render_template('eat.html', posts=posts_parsed)
 
@@ -54,3 +62,24 @@ def restaurant(post_id):
         split = menu_items[i].split(":")
         menu_items[i] = split[0] + "    $" + split[1]
     return render_template('restaurant.html', post=post, menu=menu_items)
+
+@app.route('/chef', methods=('GET', 'POST'))
+def chef():
+    if request.method == 'POST':
+        name = request.form['name']
+        phone = str(request.form['phone'])
+        menu = request.form['menuItems']
+        tags = request.form['tags']
+        genre = request.form['genre'] 
+        des = request.form['desc']
+        full_add = request.form['street'] + ", " + request.form['city'] + ", " + request.form['state'] + ", " + request.form['zip'] + ", USA"
+
+        if not name or not phone:
+            flash('Title is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO rest (name, phone, menu, tags, genre, des, full_add) VALUES (%s, %s, %s, %s, %s, %s, %s)',
+                         (name, phone, menu, tags, genre, des, full_add))
+            conn.close()
+            return redirect(url_for('eat'))
+    return render_template('chef.html')
